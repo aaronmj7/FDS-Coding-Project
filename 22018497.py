3339578cf786c8f60ddb9b8d85788d4c338b7818
Aaron M Joseph <125889154+aaronmj7@users.noreply.github.com>
2023-04-07 20:56:10 +0100

    Add files via upload

---
+++ 22018497.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  7 20:49:28 2023
+
+@author: aaron
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.genfromtxt('data7.csv', delimiter=' ')
