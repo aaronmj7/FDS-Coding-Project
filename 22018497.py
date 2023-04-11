bd67893d2a1e5bb8fe80160bfdad63649a0519dc
Aaron M Joseph <125889154+aaronmj7@users.noreply.github.com>
2023-04-11 19:57:19 +0100

    Add files via upload

--- 22018497.py
+++ 22018497.py
@@ -27,8 +27,9 @@ ydist = dist / np.sum(dist)
 avg = np.sum(xdist * ydist)
 
 # calculating the value X such that 0.75 of the distribution are below X
-# cumulative distribution
 cdist = np.cumsum(ydist)
+indx = np.argmin(np.abs(cdist - 0.75))
+X = edge[indx]
 
 
 # plotting histogram
@@ -36,8 +37,13 @@ plt.figure()
 plt.bar(xdist, ydist, width=0.9*wdist)
 
 # plotting and displaying mean
-plt.plot([avg, avg], [0.0, max(ydist)], c='red')
+plt.plot([avg, avg], [0.0, 0.075], c='red')
 text = ''' Average weight: {}kg'''.format(np.round(avg, 2))
-plt.text(x=avg, y=max(ydist) + 0.001, s=text, c='red')
+plt.text(x=2.7, y=0.075, s=text, fontsize=9, c='red')
+
+# plotting and displaying X value
+plt.plot([X, X], [0.0, max(ydist)], c='black')
+text = '''75% are born with a weight below {}kg'''.format(np.round(X, 2))
+plt.text(x=2.33, y=max(ydist), s=text, fontsize=9, c='black')
 
 plt.show()
